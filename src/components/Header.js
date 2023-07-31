cc7136f6365a70df6ae1813f39d332e426d27f8a
sharashero <sharshar.dev@gmail.com>
2023-07-31 16:38:39 +0300

    added logo

--- src/components/Header.js
+++ src/components/Header.js
@@ -12,6 +12,10 @@ const navLinks = [
 function Header() {
   return (
     <header>
+      <div className="logo">
+        <img src="Logo.svg" alt="logo" />
+      </div>
+
       <Nav links={navLinks} />
     </header>
   );
