77854c6466ec39e912d3d51d8a776c30e725d95d
sharashero <sharshar.dev@gmail.com>
2023-07-31 18:05:30 +0300

    added footer structure

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,30 @@
+import Nav from './Nav';
+
+const navLinks = [
+  { href: '#', name: 'Home' },
+  { href: '#', name: 'About' },
+  { href: '#', name: 'Menu' },
+  { href: '#', name: 'Reservations' },
+  { href: '#', name: 'Order online' },
+  { href: '#', name: 'Login' },
+];
+
 function Footer() {
   return (
     <footer>
+      <div >
+        Footer Logo
+      </div>
 
+      <Nav links={navLinks} />
+
+      <div>
+        Contacts
+      </div>
+
+      <div>
+        Social Media
+      </div>
     </footer>
   );
 }
