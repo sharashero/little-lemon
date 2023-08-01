7e9d594812136858595cf83a8d637581a748f083
sharashero <sharshar.dev@gmail.com>
2023-08-01 08:49:00 +0300

    replaced Hero component placehoder

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -6,7 +6,7 @@ function Hero() {
       <div>
         <h1>Little Lemon</h1>
         <h2>Chicago</h2>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab laboriosam dolor natus ut tenetur iusto ullam, voluptate temporibus doloribus fugiat repellat quae impedit odit ratione illum? Eligendi alias velit cumque.</p>
+        <p>A family-owned Mediterranean restaurant, focused on traditional recipes served with a modern twist. The chefs draw inspiration from Italian, Greek, and Turkish culture and have a menu of 12–15 items that they rotate seasonally.</p>
         <Button>Reserve a Table</Button>
       </div>
 
