d58b90297068d9686bb537d6ffa33fdd8c542850
sharashero <sharshar.dev@gmail.com>
2023-08-02 20:49:12 +0300

    moved available times to Main as a state

--- src/components/BookingPage.js
+++ src/components/BookingPage.js
@@ -1,15 +1,6 @@
 import BookingForm from "./BookingForm";
 
-const availableTimes = [
-  '17:00',
-  '18:00',
-  '19:00',
-  '20:00',
-  '21:00',
-  '22:00'
-];
-
-function BookingPage() {
+function BookingPage({ availableTimes }) {
   return (
     <div className="booking-page container">
       <BookingForm availableTimes={availableTimes} />
--- src/components/Main.js
+++ src/components/Main.js
@@ -1,13 +1,23 @@
 import { Route, Routes } from 'react-router-dom';
 import HomePage from "./HomePage";
 import BookingPage from "./BookingPage";
+import { useState } from 'react';
 
 function Main() {
+  const [availableTimes, setAvailableTimes] = useState([
+    '17:00',
+    '18:00',
+    '19:00',
+    '20:00',
+    '21:00',
+    '22:00'
+  ]);
+
   return (
     <main>
       <Routes>
         <Route path="/" element={<HomePage />} />
-        <Route path="/booking" element={<BookingPage />} />
+        <Route path="/booking" element={<BookingPage availableTimes={availableTimes} />} />
       </Routes>
     </main>
   );
